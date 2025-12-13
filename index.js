bb4e296cc8d2ff343848baf94cfcb983480fc6b0
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2025-12-13 20:39:44 +0600

    implement user role post

--- index.js
+++ index.js
@@ -25,6 +25,37 @@ async function run() {
     try {
         await client.connect();
 
+        //collections
+        const database = client.db('bloodDonationDB')
+        const usersCollections = database.collection('users');
+        
+
+        // user post role
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            userInfo.role = "donor"
+            userInfo.status = "active"
+            userInfo.createdAt = new Date();
+
+            const result = await usersCollections.insertOne(userInfo)
+            res.send(result)
+        })
+
+        app.get('/users/role/:email', async (req, res) => {
+            const { email } = req.params;
+            console.log(email);
+
+            const query = { email: email }
+            const result = await usersCollections.findOne(query)
+            console.log(result);
+
+            res.send(result)
+
+        })
+
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
