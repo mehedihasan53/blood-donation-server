3d20b8d4228613a55f13eb62f35b02b3fc5a323c
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2025-12-13 11:04:22 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = process.env.MONGODB_URI;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
