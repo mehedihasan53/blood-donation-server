4a70ea568fb7753971de586336d4e183025e243b
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2025-12-18 23:51:05 +0600

    solved bugs

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         //collections
         const database = client.db("bloodDonationDB");
@@ -400,7 +400,7 @@ async function run() {
             }
         );
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
